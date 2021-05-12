22cdda447232eaafefc32a4c57866f032b9c16e4
Harris Ward <harris.ward@ivy-is.co.uk>
2021-05-12 13:53:18 +0000

    Small change to the CGG Sample Chart

--- install/solution/public/SuiteCRM+Analytics/System/Resources/SAMPLES/CGGCHART.js
+++ install/solution/public/SuiteCRM+Analytics/System/Resources/SAMPLES/CGGCHART.js
@@ -2,6 +2,9 @@ lib('cdf-env.js');
 
 // http://localhost:8080/suitecrmanalytics/plugin/cgg/api/services/draw?script=/public/SuiteCRM+Analytics/System/Resources/SAMPLES/CGGCHART.js&outputType=png
 
+var dataAccessId = "KETTLEDUMMY"
+var legendVisible = true;
+
 var chart = {
 // You can change the chart type below
   type: "cccBarChart",
@@ -16,18 +19,28 @@ var chart = {
         cd = this.chartDefinition;
     
     $.extend(true,cd,analytics.opts.charts.defaultChartComp);
+    $.extend(true,cd,analytics.opts.charts.defaultBarChartComp);
 
   },
   chartDefinition:  {
-    dataAccessId: "DUMMY1",
+    dataAccessId: dataAccessId,
     path: "/public/SuiteCRM Analytics/System/Resources/SAMPLES/CHARTS.cda",
     width: 2048,
     height: 800,
-    legendVisible: false,
-    orthoAxisTitle: "Average Call Duration (Hours)",
+    legendVisible: legendVisible,
+    //legendItemPadding: 10,
+    // This padding fixes the legend truncation
+    legendPaddings: 10,
+    //legendMargins: 10,
+    orthoAxisTitle: "Average Call Duration (Minutes)",
     orthoAxisTitleAlign: "center",
-    //baseAxisTitle: "Period",
-    //baseAxisTitleAlign: "middle",
+    stacked: true,
+    seriesInRows: true,
+    crosstabMode: false,
+    baseAxisTitle: "Period",
+    baseAxisTitleAlign: "middle",
+    // Override data order
+    categoryRole: "category desc",
     compatVersion: 3
   },
   lifecycle:  {
